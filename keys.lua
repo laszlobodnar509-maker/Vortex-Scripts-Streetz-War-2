c175539501bc157ee947bc250e7e1411350a06fe
laszlobodnar509-maker <laszlobodnar509@gmail.com>
2026-08-30 11:27:47 +0200

    Create keys.lua for Vortex Scripts key management
    This file serves as a key database for Vortex Scripts, detailing key formats and duration options.

---
+++ keys.lua
@@ -0,0 +1,30 @@
+-- Vortex Scripts | Key Database
+-- Edit this file to add/remove keys
+-- Upload to your GitHub repo
+
+--[[
+    KEY FORMAT:
+    ["VXSCRIPTS-XXXX-XXXX-XXXX"] = "duration"
+    
+    DURATION OPTIONS:
+    "30m"  = 30 minutes
+    "1h"   = 1 hour
+    "24h"  = 24 hours
+    "7d"   = 7 days
+    "14d"  = 14 days
+    "30d"  = 30 days (1 month)
+    "90d"  = 90 days (3 months)
+    "365d" = 365 days (1 year)
+    "life" = lifetime (never expires)
+    
+    EXAMPLES:
+    ["VXSCRIPTS-7K2M-9P4N-XR8W"] = "life",
+    ["VXSCRIPTS-AB12-CD34-EF56"] = "7d",
+    ["VXSCRIPTS-TEST-TEST-TEST"] = "24h",
+]]
+
+return {
+    -- LIFETIME KEYS
+    ["VXSCRIPTS-7K2M-9P4N-XR8W"] = "life",
+    ["VXSCRIPTS-3H6J-5T1L-BQ4V"] = "life",
+}
